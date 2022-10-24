55d4cd65afff77c88bf33aa633152488b54f134c
ABHISHEK PATIL <abhishek.patil@joflee.com>
2022-10-24 21:59:47 +0530

    state notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,18 +23,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+extension AddisonTools<T extends num> on T? {
+  T? operator +(T? other) {
+    final shadow = this;
+    if (shadow == null) return null;
+    return shadow + (other ?? 0) as T;
+  }
+}
+
+class CounterNotifier extends StateNotifier<int?> {
+  CounterNotifier() : super(null);
+  void increment() => state = (state == null ? 1 : state + 1);
+}
+
+final counterProvider = StateNotifierProvider<CounterNotifier, int?>(
+  (ref) => CounterNotifier(),
+);
+
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomePage'),
+        title: Consumer(
+          builder: (context, ref, child) {
+            final counter = ref.watch(counterProvider);
+            final text = counter?.toString() ?? 'Press the button';
+            return Text(
+              text,
+              style: Theme.of(context).textTheme.headline3,
+            );
+          },
+        ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('hi'),
+      body: Center(
+        child: TextButton.icon(
+          onPressed: ref.read(counterProvider.notifier).increment,
+          icon: const Icon(Icons.add),
+          label: const Text(
+            'add',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+        ),
       ),
     );
   }
