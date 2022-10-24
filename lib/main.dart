4cbc9d6b1df3a55595959a05e1a9a48370c321d4
ABHISHEK PATIL <abhishek.patil@joflee.com>
2022-10-24 23:19:58 +0530

    stream provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
+
+const names = [
+  'Talia',
+  'Lyla',
+  'Billie',
+  'Lila',
+  'Lance',
+  'Noemy',
+  'Malika',
+  'Emmanuel',
+  'Shaniya',
+  'Maverick',
+  'Charley',
+];
+
+final tickerProvider = StreamProvider<int>(
+  (ref) => Stream.periodic(
+    const Duration(seconds: 1),
+    (computationCount) => computationCount + 1,
+  ),
+);
+
+final namesProvider = StreamProvider(
+  (ref) => ref.watch(tickerProvider.stream).map(
+        (i) => names.getRange(0, i),
+      ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -28,8 +60,20 @@ class MyHomePage extends StatelessWidget {
         title: const Text('HomePage'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('hi'),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final names = ref.watch(namesProvider);
+          return names.when(
+            data: (names) => ListView.builder(
+              itemCount: names.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Text(names.elementAt(index));
+              },
+            ),
+            error: (_, __) => const Text('Reached end of list'),
+            loading: () => const Text('Loading...'),
+          );
+        },
       ),
     );
   }
